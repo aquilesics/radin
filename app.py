ec6fc8c0f1143b139298e68c2335fbbcac414c95
aquilesics <aquiles.cout@gmail.com>
2023-06-13 21:34:46 -0300

    add css styles

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app.mount('/static',StaticFiles(directory = 'static'), name='static')
 
 templates = Jinja2Templates(directory="templates")
 
+@app.get('/',response_class = HTMLResponse )
 @app.get('/home',response_class = HTMLResponse )
 async def home(request:Request):
     return templates.TemplateResponse("index.html",{'request':request })
